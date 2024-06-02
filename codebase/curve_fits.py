3c05f4190635e74ef40fbb8c16ba1793082cbbfb
James Maze <jmaze@uoregon.edu>
2024-06-02 15:03:44 -0700

    Curve fitting went way better after a night's rest

--- codebase/curve_fits.py
+++ codebase/curve_fits.py
@@ -14,137 +14,111 @@ import matplotlib.pyplot as plt
 import scipy.stats as stats
 import os
 
+from scipy.optimize import curve_fit
+
 os.chdir("/Users/jmaze/Documents/geog590/")
 
-# %% 2.0 Read claims data
-
+#Read claims data
 claims_v2 = pd.read_csv('./project_data/claims_v2.csv')
-storms = claims_v2[claims_v2['hazard_broad'] == 'GeneralStorm']
+
+# %% 2.0 Model RI for Total Annual Storm Damages
+
+storms = claims_v2[claims_v2['hazard_broad'] == 'GeneralStorm'].copy()
 #storms = claims_v2.copy()
 
-storms_dates = storms.groupby(['StartDate', 'EndDate']).agg(
-    total_cnty_dmg=('PropertyDmg(ADJ)', 'sum'), 
-    num_counties=('CountyName', 'nunique'),
-    counties_list=('CountyName', lambda x: x.unique().tolist())
+storms.loc[:, 'property_dmg'] = storms['PropertyDmg(ADJ)'] / 1e6 
+
+storms_years = storms.groupby('Year').agg(
+    total_annual_dmg=('property_dmg', 'sum')
 ).reset_index()
 
-storms_dates['total_cnty_dmg'] = storms_dates['total_cnty_dmg'] / 1e6
+storms_years['rank'] = storms_years['total_annual_dmg'].rank(ascending=False).astype(int)
+
+# Calculate the Return Period = (n+1/m)
+def calc_ri(dataframe, year_col, damage_col):
+    df = dataframe.copy()
+    
+    # Rank the years based on the damage column in descending order
+    df['rank'] = df[damage_col].rank(ascending=False).astype(int)
+    
+    # Calculate the number of years in the record
+    record_yrs = df[year_col].max() - df[year_col].min()
+
+    # Define the function to calculate Return Interval (RI)
+    def ri_calc(row):
+        rank = row['rank']
+        ri = (record_yrs + 1) / rank
+        return ri
+
+    # Apply the function to each row
+    df['RI'] = df.apply(ri_calc, axis=1)
+    
+    return df
+
+    
+storms_years = calc_ri(storms_years, 'Year', 'total_annual_dmg')
+
+# %% 2.1 Log fit to t
+
+modeling_domain = np.linspace(1, 100, num=1000)
+
+def log_func(x, a, b):
+    return a * np.log(x) + b
 
 
-# %% 3.0
+def ri_model_fitting(df, ri_col, dmg_col):
+    
+    model = curve_fit(log_func, df[ri_col], df[dmg_col])
+    params = model[0]
+    covariance = model[1]
 
-# Convert 'StartDate' to datetime
-storms_dates['StartDate'] = pd.to_datetime(storms_dates['StartDate'])
+    a_fit, b_fit = params
+    
+    model_dmg = log_func(modeling_domain, params[0], params[1])
+    
+    return model_dmg
 
-# Filter the data
-storms_early = storms_dates[storms_dates['StartDate'] < '1991-01-01']
-storms_late = storms_dates[storms_dates['StartDate'] > '1991-01-01']
+curve = ri_model_fitting(storms_years, 'RI', 'total_annual_dmg')
 
-# Create subplots
-fig, (ax1, ax2) = plt.subplots(nrows=1, ncols=2, figsize=(16, 8))  # Adjust figsize for better spacing
+plt.scatter(storms_years['RI'], storms_years['total_annual_dmg'], label='Data', color='navy')
 
-# Plot histogram for storms_early
-ax1.hist(storms_early['total_cnty_dmg'], bins=40, edgecolor='black')
-ax1.set_yscale('log')
-ax1.set_title('Storms Before 1991')
-
-# Plot histogram for storms_late
-ax2.hist(storms_late['total_cnty_dmg'], bins=40, color='orange', edgecolor='black')
-ax2.set_yscale('log')
-ax2.set_title('Storms After 1991')
-
-plt.tight_layout()
+# Plot the fitted log curve
+plt.plot(modeling_domain, curve, label='Fitted Curve', color='orange')
+plt.xlabel('Recurrance Interval (Years)')
+plt.ylabel('Inflation adjusted damages ($Million 2022 dollars)', size = 8)
+plt.legend(loc='lower right')
 plt.show()
 
-# %% 4.0 Try curve fitting
+# %% 5.0 Model the RI for Per Capita Storm Damages
 
-# Fit a Log-Normal distribution to the data
-shape, loc, scale = stats.lognorm.fit(storms_dates['total_cnty_dmg'], floc=0)
-print(f"Fitted parameters: shape={shape}, loc={loc}, scale={scale}")
+storms_years_percap = storms.groupby('Year').agg(
+    annual_dmg_percap=('PropertyDmgPerCapita', 'sum')
+).reset_index()
 
-# Generate a range of values over which to evaluate the PDF
-x = np.linspace(storms_dates['total_cnty_dmg'].min() + 0.25, storms_dates['total_cnty_dmg'].max(), 1000)
-fitted_pdf = stats.lognorm.pdf(x, shape, loc, scale)
+storms_years_percap = calc_ri(storms_years_percap, 'Year', 'annual_dmg_percap')
 
-# %% 5.0
+curve = ri_model_fitting(storms_years_percap, 'RI', 'annual_dmg_percap')
 
-fig, ax = plt.subplots(figsize=(8, 8))
+plt.scatter(storms_years_percap['RI'], storms_years_percap['annual_dmg_percap'], label='Data', color='navy')
+plt.plot(modeling_domain, curve, label='Fitted Curve', color='orange')
+plt.xlabel('Recurrance Interval (Years)')
+plt.ylabel('Per-capita inflation ajusted damages ($)')
+plt.legend(loc='lower right')
 
-hist_data, bins, _ = plt.hist(storms_dates['total_cnty_dmg'], bins=50, edgecolor='black', density=False)
+# %% Evaluate whether inflation adjusted per-capita damages change over time.
 
-# Calculate the bin width
-bin_width = bins[1] - bins[0]
+modeling_domain = np.linspace(1, 50, num=1000)
 
-# Scale the PDF by the number of data points and the bin width
-scaled_pdf = fitted_pdf * len(storms_dates['total_cnty_dmg']) * bin_width
+early = storms_years_percap[storms_years_percap['Year'] < 1991]
+early = early[early['Year'] != 1984]
+early = calc_ri(early, 'Year', 'annual_dmg_percap')
+curve_early = ri_model_fitting(early, 'RI', 'annual_dmg_percap')
 
-# Plot the scaled PDF
-ax.plot(x, scaled_pdf, 'r-', lw=2, label='Fitted Log-Normal PDF')
+late = storms_years_percap[storms_years_percap['Year'] >= 1991]
+late = calc_ri(late, 'Year', 'annual_dmg_percap')
+curve_late = ri_model_fitting(late, 'RI', 'annual_dmg_percap')
 
-
-# Set y-axis to logarithmic scale (uncomment if needed)
-ax.set_yscale('log')
-
-
-
-plt.show()
-
-# %%
-
-# Fit a Gamma distribution to the data
-alpha, loc, beta = stats.gamma.fit(storms_dates['total_cnty_dmg'], floc=0)
-print(f"Fitted parameters: alpha={alpha}, loc={loc}, beta={beta}")
-
-# Generate a range of values over which to evaluate the PDF
-x = np.linspace(storms_dates['total_cnty_dmg'].min(), storms_dates['total_cnty_dmg'].max(), 1000)
-fitted_pdf = stats.gamma.pdf(x, alpha, loc, beta)
-
-# Plot the histogram of the data
-plt.hist(storms_dates['total_cnty_dmg'], bins=50, edgecolor='black', density=True, alpha=0.6)
-
-# Plot the fitted Gamma PDF
-plt.plot(x, fitted_pdf, 'r-', lw=2, label='Fitted Gamma PDF')
-
-# Set y-axis to logarithmic scale (uncomment if needed)
-# plt.yscale('log')
-
-plt.xlabel('Total County Damage')
-plt.ylabel('Density')
-plt.yscale('log')
-plt.title('Fitting a Gamma Distribution')
-plt.legend()
-plt.show()
-
-# %%
-
-
-# Assuming storms_dates['total_cnty_dmg'] is your data
-
-# Fit a Pareto distribution to the data
-b, loc, scale = stats.pareto.fit(storms_dates['total_cnty_dmg'], floc=0)
-print(f"Fitted Pareto parameters: b={b}, loc={loc}, scale={scale}")
-
-# Generate a range of values over which to evaluate the PDF
-x = np.linspace(storms_dates['total_cnty_dmg'].min() + 1, storms_dates['total_cnty_dmg'].max()+ 10, 1000)
-fitted_pdf_pareto = stats.pareto.pdf(x, b, loc, scale)
-
-# Plot the histogram and the fitted PDF
-fig, ax = plt.subplots(figsize=(8, 8))
-
-hist_data, bins, _ = ax.hist(storms_dates['total_cnty_dmg'], bins=50, edgecolor='black', density=False)
-
-# Calculate the bin width
-bin_width = bins[1] - bins[0]
-
-# Scale the PDF by the number of data points and the bin width
-scaled_pdf_pareto = fitted_pdf_pareto * len(storms_dates['total_cnty_dmg']) * bin_width
-
-# Plot the scaled PDF
-ax.plot(x, scaled_pdf_pareto, 'r-', lw=2, label='Fitted Pareto PDF')
-
-# Set y-axis to logarithmic scale
-ax.set_yscale('log')
-
-# Add legend
-ax.legend()
-
+plt.plot(modeling_domain, curve_early, label='1960-1991', color ='skyblue')
+plt.plot(modeling_domain, curve_late, label='1991-2022', color='red')
+plt.scatter(early['RI'], early['annual_dmg_percap'], label='Early Data', color='navy')
+plt.scatter(late['RI'], late['annual_dmg_percap'], label='Late Data', color='maroon')
